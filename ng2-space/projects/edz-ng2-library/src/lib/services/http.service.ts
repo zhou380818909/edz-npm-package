1a9df8f6f37b987ccf2023ac81301542810009a0
ChouEric <choueric@foxmail.com>
2020-07-30 20:32:44 +0800

    修复http服务的一个bug

--- ng2-space/projects/edz-ng2-library/src/lib/services/http.service.ts
+++ ng2-space/projects/edz-ng2-library/src/lib/services/http.service.ts
@@ -2,7 +2,7 @@
  * @Author: ChouEric
  * @Date: 2020-07-15 15:05:59
  * @Last Modified by: ChouEric
- * @Last Modified time: 2020-07-30 20:02:49
+ * @Last Modified time: 2020-07-30 20:30:31
  * @Description: 封装 http 请求
  */
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
@@ -291,7 +291,8 @@ export class HttpService {
       return EMPTY
     }
     if (error && error.status) {
-      const { status, error: { text } = {} } = error as HttpErrorResponse
+      const { status, error: errorData } = error as HttpErrorResponse
+      const { text } = errorData || {}
       if (status <= 200) {
         // 如果返回的是包含 html 关键字的文本, 则认为是重定向到了登录页
         if (/<html/i.test(text)) {
